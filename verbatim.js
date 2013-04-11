cf36d97c23066d800cbfbfbd9e04c7e3161a91bc
Kjell <kjell@leanside.com>
2013-04-11 15:43:44 -0500

    Touch: tap/pinchOut opens figures, pinchIn closes

---
+++ verbatim.js
@@ -0,0 +1,53 @@
+var h = Hammer(document)
+var debug = window.location.host.match(/local/)
+
+var figure = function(element) {
+  var e = element
+  while(e.parentNode.nodeName != 'BODY') {
+    if(e.nodeName == 'FIGURE') break
+    e = e.parentNode
+  }
+  return e
+}
+
+var touched = function(f) {
+  if(debug) console.log(event, event.type) && (window.e = event)
+  event.preventDefault()
+  fig = new Figure(event.target)
+  if(f != undefined) f(fig)
+}
+
+// Creates a Figure wrapping one of the <figures>
+//   element: the <figure> element or any of it's child nodes
+//   returns:
+function Figure(element) {
+  this.element = figure(element)
+}
+
+Figure.prototype.open = function() {
+  Figure.closeAll()
+  history.pushState({}, '', '#' + this.element.getAttribute('id'))
+  this.element.classList.add('open')
+}
+Figure.prototype.close = function() {
+  this.element.classList.remove('open')
+}
+Figure.closeAll = function() {
+  history.replaceState({}, '', '#');
+  [].forEach.call(document.querySelectorAll("figure.open"), function(fig) {
+    (new Figure(fig)).close()
+  })
+}
+
+
+h.on("touch", function(event) {
+  touched(function(fig) { fig.open() })
+});
+
+h.on("pinchin", function(event) {
+  touched(function() { fig.close() })
+});
+
+h.on("pinchout", function(event) {
+  touched(function(fig) { fig.open() })
+});
